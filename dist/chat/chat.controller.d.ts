3cddf1ca18684aaeed85eb393c635297844588dd
StartOff123 <startoff123@mail.ru>
2023-10-15 23:35:50 +0500

    first commit

--- dist/chat/chat.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { ChatService } from './chat.service';
-export declare class ChatController {
-    private chatService;
-    constructor(chatService: ChatService);
-    getMessages(res: any): Promise<void>;
-}
--- dist/chat/chat.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Message, Prisma } from '@prisma/client';
-import { PrismaService } from '../prisma.service';
-export declare class ChatService {
-    private prismaService;
-    constructor(prismaService: PrismaService);
-    createMessage(data: Prisma.MessageCreateInput): Promise<Message>;
-    getMessages(): Promise<Message[]>;
-}
